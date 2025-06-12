3d1d1ecd9199240e9a6e583edfa56dd7aef9eea5
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-12 17:48:02 +0700

    Tahap 8

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -54,5 +54,17 @@ class Queues
                 cout << "Queue underflow\n"; 
                 return;                        
             }
+            cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+            // Cek jika antrian hanya memiliki satu elemen
+            if (FRONT == REAR)
+            {
+                FRONT = -1;
+                REAR = -1;
+            }
+            else
+            {
+                
+            }
         }
 }
