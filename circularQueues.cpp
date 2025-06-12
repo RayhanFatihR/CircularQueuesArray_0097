01805a906093ee854e41a644d0ac4cf05a615fc1
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-12 18:34:50 +0700

    Tahap 14

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -70,7 +70,7 @@ class Queues
                 else
                     FRONT = FRONT + 1;
             }
-
+        }
             void display()
             {
                 int FRONT_position = FRONT;
@@ -92,6 +92,35 @@ class Queues
                     {
                         cout << queue_array[FRONT_position] << " ";
                         FRONT_position++;
+                    }
+                    cout << endl;
+                }
+                else 
+                {
+                    //Jika FRONT_position > REAR_position, interasi dari FRONT hingga akhir array
+                    while (FRONT_position <= max - 1)
+                    {
+                        cout << queue_array[FRONT_position] << " ";
+                        FRONT_position++;
+                    }
+                    else
+                    {
+                        //Jika FRONT_position > REAR_position, interasi dari FRONT hingga akhir array
+                        while (FRONT_position <= max - 1)
+                        {
+                            cout << queue_array[FRONT_position] << " ";
+                            FRONT_position++;
+
+                            FRONT_position = 0; 
+
+                            //Interasi dari awal array hingga REAR
+                            while (FRONT_position <= REAR_position)
+                            {
+                                cout << queue_array[FRONT_position] << " ";
+                                FRONT_position++;
+                            }
+                            cout << endl;
+                        }
                     }
                 }
             }
