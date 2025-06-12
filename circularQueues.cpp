3308dd420ecd1beb02a46622e6f4fdb06de2eff3
RayhanFatihR <rayhanfatihr76@gmail.com>
2025-06-12 18:53:22 +0700

    Tahap 20

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -160,8 +160,14 @@ int main()
                 default:
                 {
                     cout << "Invalid option!!" << endl;
+                    break;
                 }
             }
-        }
+        }   
+        catch (exception& e)
+            {
+                cout << "Check for the values entered." << endl;
+            }
     }
+    return 0;
 }
